4a9ded5583ab1fbaccece2f532ee4299fd0294ff
xyc <zhi.chen@concentrix.ocm>
2023-03-09 17:13:50 +0800

    first2 commit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,5 @@
 import Layout from "../../components/layout";
+
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from '../../styles/utils.module.css';
